4b3a0112bdaf857979f70c2e74e449d681e2ce30
Cheri <avyishi@gmail.com>
2015-04-03 19:21:52 -0400

    Associated user and notes with each other

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,14 +6,14 @@ class NotesController < ApplicationController
   end
 
   def new
-    @note = Note.new
+    @note = current_user.notes.build
   end
-
+  
   def show
   end
 
   def create
-    @note = Note.new(note_params)
+    @note = current_user.notes.build(note_params)
     if @note.save
       redirect_to @note
     else
